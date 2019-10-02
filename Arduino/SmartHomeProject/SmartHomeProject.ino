2a46e68de70fe1a65b6ab58609d16eaa8eee12dc
Samhith Sripada <20867852+samhithsripada@users.noreply.github.com>
2019-10-02 12:36:18 -0400

    Organized File

---
+++ Arduino/SmartHomeProject/SmartHomeProject.ino
@@ -0,0 +1,17 @@
+int photoLED = 13;
+
+void setup() {
+
+  pinMode(A0, INPUT);
+  pinMode(photoLED, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  int sensorVal = analogRead(A0);
+  Serial.println(sensorVal);
+  digitalWrite(photoLED, HIGH);
+  
+}
